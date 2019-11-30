2b5219b91baadba182be4d29a92ad8c4b68850ef
Javed Ali Dahri <Javed Ali Dahri@192.168.10.24>
2019-11-30 12:55:14 +0500

    This is dbconnection. Use your own username and password and database

--- LibraryManagementSystem/src/com/geeks/lms/connection/DBConnection.java
+++ LibraryManagementSystem/src/com/geeks/lms/connection/DBConnection.java
@@ -1,5 +1,21 @@
 package com.geeks.lms.connection;
 
-public class DBConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DBConnection {
+	private DBConnection(){}
+	public static Connection con=null;
+	public static Connection getConnection(){
+		if(con==null){
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/library_management_system","username","password");
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		return con;
+	}
 }
